e4cdde50d19ead7a578c74a09447657863e9cdda
LoudBar <71870985+LoudBar@users.noreply.github.com>
2020-10-13 15:29:34 +0300

    Update Concat.java

--- Concat.java
+++ Concat.java
@@ -2,15 +2,19 @@
 // (для массивов {1,3,5} и {2,4,6} будет результат {1,3,5,2,4,6}).
 package com.company;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void concat(int[] array1, int[] array2) {
-        String a = Arrays.toString(array1).replaceAll("\\[|\\]|,|\\s", "");
-        String b = Arrays.toString(array2).replaceAll("\\[|\\]|,|\\s", "");
-        String ans = a.concat(b);
-        System.out.println(ans);
+        int[] array3 = new int[array1.length + array2.length];
+        for (int i = 0; i < array1.length; i++) {
+            array3[i] = array1[i];
+        }
+        for (int i = array1.length; i < array3.length; i ++) {
+            array3[i] = array2[i- array1.length];
+        }
+        for (int i = 0; i < array3.length; i++) {
+            System.out.println(array3[i]);
+        }
     }
 
     public static void main(String[] args) {
